6fadfe011c23e2d1d315030aeefdc92991bcad1a
nathan <ahrl_ahrl@naver.com>
2020-04-10 11:03:15 +0900

    react 셋업, router 작업

---
+++ src/Components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header';
+import Router from './Router';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Router />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default () => (
+  <div>
+    <ul>
+      <li>
+        <a href='Home'>Home</a>
+      </li>
+      <li>
+        <a href='TV'>TV</a>
+      </li>
+      <li>
+        <a href='Search'>Search</a>
+      </li>
+    </ul>
+  </div>
+);
